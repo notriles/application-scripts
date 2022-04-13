21ae70530817aa04ce41b52952baa5039984fbf9
notriles <86499888+notriles@users.noreply.github.com>
2022-04-13 16:53:29 -0400

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -5,21 +5,21 @@ enum class color { BROWN, BLOND, BLACK }; // create enum for hair color
 
 class Person
 {
-  // initialize name, age, and hair color
+  	// initialize name, age, and hair color
 	std::string name;
 	int age;
 	color hair;
 public:
-  //constructor with three parameters
+  	//constructor with three parameters
 	Person(std::string name, int age, color hair)
 	{
-    // set each parameter to the appropriate property
+    	// set each parameter to the appropriate property
 		this->name = name;
 		this->age = age;
 		this->hair = hair;
 	}
   
-  // create getters to access private members in other areas
+  	// create getters to access private members in other areas
 	std::string getName()
 	{
 		return name;
@@ -35,7 +35,7 @@ public:
 		return hair;
 	}
 
-  // create setters to change the private members from other areas
+  	// create setters to change the private members from other areas
 	void setName(std::string name)
 	{
 		this->name = name;
@@ -54,8 +54,8 @@ public:
 
 int main()
 {
-  // create a new Person object with random attributes
+  	// create a new Person object with random attributes
 	Person Bob = Person("Robert", 23, color::BROWN);
-  // print his name to the console
+  	// print his name to the console
 	std::cout << Bob.getName();
 }
